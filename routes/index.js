e1ef1d1d812174f715e0aba7246d4f8b50e60607
Vini Laz <Vini@MacBook-Pro-de-Vini.local>
2019-06-14 16:16:42 +0200

    weatherapp part3 fini

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
  var cityList = [
-  {name:'Paris',desc:"couvert",img:'./images/picto-1.png',temp_min : 18,temp_max : 24},
-  {name:'Lyon',desc:'soleil', img: './images/picto-1.png',temp_min : 14,temp_max : 24},
-  {name:'Marseille',desc:'nuageux',img:'./images/picto-1.png',temp_min: 0,temp_max:30},
+//   {name:'Paris',desc:"couvert",img:'./images/picto-1.png',temp_min : 18,temp_max : 24},
+//   {name:'Lyon',desc:'soleil', img: './images/picto-1.png',temp_min : 14,temp_max : 24},
+//   {name:'Marseille',desc:'nuageux',img:'./images/picto-1.png',temp_min: 0,temp_max:30},
  ];
 
 
@@ -14,13 +15,22 @@ router.get('/', function(req, res, next) {
 
 router.post('/add-city', function(req, res, next) {
     console.log("CITY PUSH HANDLED! ----> ", req.body.addedCityFromFront)
-    // cityList.push(
-    //   {
-    //     name: name
+   request(' http://api.openweathermap.org/data/2.5/weather?q='+req.body.addedCityFromFront+'&appid=f559102f7a2b5841d48a199d217b7aa8',function (error,response,body){
+    body=JSON.parse(body);
+    console.log("body",body);
+
+    cityList.push({
+    name: body.name,
+    desc:body.weather[0].description,
+    img: 'http://openweathermap.org/img/w/'+body.weather[0].icon+'.png',
+    temp_min:body.main.temp_min,
+    tem_max:body.main.tem_max
+
+    })
   
-    //   }
-    // )
-    res.render('index', { cityList});
+             res.render('index', { cityList});
+
+        });
   });
 
 
